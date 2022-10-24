64f58332ed42ce6f02ee2389453063ae72410925
angelsflyinhell <azami@namespace.media>
2022-10-24 21:56:20 +0200

    Navbar fun

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 export default function Navbar() {
+
+    const [hamClass, setHamClass] = useState("mobile-menu hidden");
+
+    const toggleHam = () => {
+        if (hamClass === "mobile-menu hidden") {
+            setHamClass("mobile-menu");
+        } else {
+            setHamClass("mobile-menu hidden");
+        }
+    }
+
     return <>
-        <nav className="bg-gray-100 font-bold text-md">
-            <div className="max-w-7xl mx-auto">
+        <nav className="text-md font-bold">
+            <div className="max-w-7xl mx-auto px-4 py-4 md:py-2">
                 <div className="flex justify-between">
                     <div className="flex items-center space-x-4">
                         <a href="#" className="flex space-x-2">
@@ -11,18 +24,31 @@ export default function Navbar() {
                             <span>angelsflyinhell</span>
                         </a>
                     </div>
-                    <div className="flex items-center space-x-4">
-                        <a href="#" className="py-3 px-3">Research</a>
+                    <div className="hidden md:flex items-center space-x-4">
+                        <a href="#" className="py-3 px-3">Sandbox</a>
                         <a href="#" className="py-3 px-3">Projects</a>
                         <a href="#" className="py-3 px-3">Blog</a>
                         <a href="#" className="py-3 px-3">About</a>
                     </div>
+
+                    <div className="md:hidden flex items-center">
+                        <button className="mobile-menu-button" onClick={toggleHam}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+                            </svg>
+                        </button>
+                    </div>
+
                 </div>
             </div>
-        </nav>
 
-        <div className="py-32 text-center">
-            <h2 className="font-extrabold text-6xl">Navbar</h2>
-        </div>
+            {/* mobile view */}
+            <div className={hamClass}>
+                <a href="" className="block py-2 px-4">Sandbox</a>
+                <a href="#" className="block py-2 px-4">Projects</a>
+                <a href="#" className="block py-2 px-4">Blog</a>
+                <a href="#" className="block py-2 px-4">About</a>
+            </div>
+        </nav>
     </>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,19 @@ import Navbar from '../components/Navbar'
 const Home: NextPage = () => {
   return (
     <>
-      <Navbar/>
+      <div className='bg-black text-white h-screen'>
+        <Navbar />
+        <div className='flex justify-center text-center'>
+          <div className="py-32 m-auto">
+            <div>
+              <h2 className="font-extrabold text-6xl">Founder of XP.</h2>
+            </div>
+            <div>
+              <p className='text-xl max-w-md py-4 text-gray-300'>8 years experience, full stack. Interested in web development, machine learning and always excited to learn.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
